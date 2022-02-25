061b18c2da33e6f75834b4188121e2dd32770ad9
Imawesome <dishapan@gmail.com>
2022-02-25 18:04:33 +0000

    added GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Disha Pandit and is{" "}
+        <a
+          href="https://github.com/DishaDarpan/react-weather-app"
+          target="_blank"
+        >
+          Open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
