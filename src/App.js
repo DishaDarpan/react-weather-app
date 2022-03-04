953db686fec92845c922fcb516151008a358e357
Imawesome <dishapan@gmail.com>
2022-03-04 17:40:47 +0000

    Created FromattedDate.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import "./App.css";
+import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
-        <Weather />
+        <Weather defaultCity="New York" />
+
         <footer>
-          This project was coded by Disha Pandit and is{" "}
+          This project was coded by{" "}
           <a
-            href="https://github.com/DishaDarpan/react-weather-app"
+            href="https://www.delac.io/"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
-            Open-sourced on GitHub
+            Matt Delac
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
